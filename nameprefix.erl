c479d97e48b249c360ecc910d5206dfff1bcdde3
sumant1122 <sumant1122@gmail.com>
2020-12-02 23:46:18 +0530

    First Examples

---
+++ nameprefix.erl
@@ -0,0 +1,9 @@
+-module(nameprefix).
+-export([greet/2]).
+
+greet(male, Name) ->
+	io:format("Hello, Mr. ~s!",[Name]);
+greet(female, Name) ->
+	io:format("Hello, Mrs. ~s!",[Name]);
+greet(_, Name) ->
+	io:format("Hello, ~s!",[Name]).
